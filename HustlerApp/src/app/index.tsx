edc5b86db4f16c3b8fd0dc2e8e0298461a70f7dc
nishthad-maker <nishtha.dubey1024@gmail.com>
2026-06-24 11:01:28 -0400

    Added 3 buttons to homescreen

--- HustlerApp/src/app/index.tsx
+++ HustlerApp/src/app/index.tsx
@@ -4,6 +4,18 @@ export default function HomeScreen() {
     <View style={styles.container}>
       <Text style={styles.title}>HUSTLER</Text>
       <Text style={styles.tagline}>Train Hard. Stay Consistent.</Text>
+    <TouchableOpacity style={styles.card}>
+      <Text style={styles.cardTitle}>Lower Body Day</Text>
+      <Text style={styles.cardSub}>Quads, Hamstrings, Glutes</Text>
+    </TouchableOpacity>
+    <TouchableOpacity style={styles.card}>
+      <Text style={styles.cardTitle}>Upper Body Day</Text>
+      <Text style={styles.cardSub}>Chest, Back, Shoulders</Text>
+    </TouchableOpacity>
+    <TouchableOpacity style={styles.card}>
+      <Text style={styles.cardTitle}>Core Day</Text>
+      <Text style={styles.cardSub}>Abs, Obliques, Lower Back</Text>
+    </TouchableOpacity>
     </View>
   );
 }
@@ -24,5 +36,23 @@ const styles = StyleSheet.create({
     fontSize: 14,
     color: '#A89880',
     marginTop: 8 ,
-  },  
+  }, 
+  card: {
+    backgroundColor: '#1E1E1E',
+    borderRadius: 12,
+    borderWidth: 1,
+    borderColor: '#FF6B00',
+    padding: 16,
+    marginTop: 20,
+  },
+  cardTitle: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: '#FFFFFF',
+  },
+  cardSub: {
+    fontSize: 13,
+    color: '#A89880',
+    marginTop: 4,
+  }, 
 });
